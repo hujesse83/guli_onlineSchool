5d93b6cb94d289a86a16cd66056a8c5fd0042a03
hujesse83 <jesse.hu@liveramp.com>
2022-01-14 15:36:51 +0800

    md5 salt

---
+++ common/common_utils/src/main/java/com/atguigu/commonutils/MD5Util.java
@@ -0,0 +1,122 @@
+package com.atguigu.commonutils;
+
+import java.util.Objects;
+import java.util.Random;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
+
+/**
+ * MD5工具类，加盐
+ *          Java实现MD5的随机加盐加密，这样以来就很难解密了，必须使用原密码才能正常的登录系统了，
+ *          以下为Java实现的MD5随机加盐加密，以及使用Apache的Hex类实现Hex(16进制字符串和)和字节数组的互转：
+ */
+public class MD5Util {
+    public static String MD5(String input) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            return "check jdk";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+        char[] charArray = input.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuilder hexValue = new StringBuilder();
+        for (byte md5Byte : md5Bytes) {
+            int val = ((int) md5Byte) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+
+
+    /**
+     * 加盐MD5
+     */
+    public static String generate(String password) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder(16);
+        sb.append(r.nextInt(99999999)).append(r.nextInt(99999999));
+        int len = sb.length();
+        if (len < 16) {
+            for (int i = 0; i < 16 - len; i++) {
+                sb.append("0");
+            }
+        }
+        String salt = sb.toString();
+        password = md5Hex(password + salt);
+        char[] cs = new char[48];
+        for (int i = 0; i < 48; i += 3) {
+            assert password != null;
+            cs[i] = password.charAt(i / 3 * 2);
+            char c = salt.charAt(i / 3);
+            cs[i + 1] = c;
+            cs[i + 2] = password.charAt(i / 3 * 2 + 1);
+        }
+        return new String(cs);
+    }
+
+    /**
+     * 校验加盐后是否和原文一致
+     */
+    public static boolean verify(String password, String md5) {
+        char[] cs1 = new char[32];
+        char[] cs2 = new char[16];
+        for (int i = 0; i < 48; i += 3) {
+            cs1[i / 3 * 2] = md5.charAt(i);
+            cs1[i / 3 * 2 + 1] = md5.charAt(i + 2);
+            cs2[i / 3] = md5.charAt(i + 1);
+        }
+        String salt = new String(cs2);
+        return Objects.equals(md5Hex(password + salt), new String(cs1));
+    }
+
+    /**
+     * 获取十六进制字符串形式的MD5摘要
+     */
+    private static String md5Hex(String src) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bs = md5.digest(src.getBytes());
+            return new String(new Hex().encode(bs));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static void main(String args[]) {
+        // 原文
+        String plaintext = "111111";
+        //  plaintext = "123456";
+        System.out.println("原始：" + plaintext);
+        System.out.println("普通MD5后：" + MD5Util.MD5(plaintext));
+
+        // 获取加盐后的MD5值
+        String ciphertext = MD5Util.generate(plaintext);
+        System.out.println("加盐后MD5：" + ciphertext);
+        System.out.println("是否是同一字符串:" + MD5Util.verify(plaintext, ciphertext));
+        /**
+         * 其中某次DingSai字符串的MD5值
+         */
+        String[] tempSalt = {"c4d980d6905a646d27c0c437b1f046d4207aa2396df6af86", "66db82d9da2e35c95416471a147d12e46925d38e1185c043", "61a718e4c15d914504a41d95230087a51816632183732b5a"};
+
+        for (String temp : tempSalt) {
+            System.out.println("是否是同一字符串:" + MD5Util.verify(plaintext, temp));
+        }
+        String a = "96e79218965eb72c92a549dd5a330112";
+        String slatA = "44f836f42906964f7d659b01d62d19c2f71e38e974e53798";
+        System.out.println(verify("96e79218965eb72c92a549dd5a330112",slatA));
+    }
+
+}
