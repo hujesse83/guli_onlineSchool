ab5b3e07526c4464fbf10f8e4863e56ad99a09f8
hujesse83 <jesse.hu@liveramp.com>
2022-01-22 11:06:30 +0800

    权限管理

---
+++ service/service_acl/src/main/java/com/atguigu/aclservice/entity/Role.java
@@ -0,0 +1,47 @@
+package com.atguigu.aclservice.entity;
+
+import com.atguigu.servicebase.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("acl_role")
+@ApiModel(value="Role对象", description="")
+public class Role extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @ApiModelProperty(value = "角色名称")
+    private String roleName;
+
+    @ApiModelProperty(value = "角色编码")
+    private String roleCode;
+
+    @ApiModelProperty(value = "备注")
+    private String remark;
+
+    @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
+    private Boolean isDeleted;
+
+
+
+}
---
+++ service/service_acl/src/main/java/com/atguigu/aclservice/entity/UserRole.java
@@ -0,0 +1,42 @@
+package com.atguigu.aclservice.entity;
+
+import com.atguigu.servicebase.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("acl_user_role")
+@ApiModel(value="UserRole对象", description="")
+public class UserRole extends BaseEntity  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "角色id")
+    private String roleId;
+
+    @ApiModelProperty(value = "用户id")
+    private String userId;
+
+    @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
+    private Boolean isDeleted;
+
+
+}
