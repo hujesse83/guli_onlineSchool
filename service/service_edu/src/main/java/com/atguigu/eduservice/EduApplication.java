a6eb31cf24ab70d9a9688d46b10ada3f9488222c
hujesse83 <jesse.hu@liveramp.com>
2022-02-02 15:24:21 +0800

    refactor pom

--- service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu"})   // 需要扫描到common包下的配置
